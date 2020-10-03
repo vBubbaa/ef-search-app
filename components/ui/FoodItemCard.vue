25ac17d46d502612b02773d7f1cca7bec058fdd0
vBubbaa <itylerjames@gmail.com>
2020-10-03 13:40:13 -0700

    Truncate long Desc1 names in fooditemcard

--- components/ui/FoodItemCard.vue
+++ components/ui/FoodItemCard.vue
@@ -1,21 +1,21 @@
 <template>
-  <div
-    class="w-full sm:w-5/12 md:5/12 p-2 mb-2 border-2 border-green-400 h-48 truncate"
-  >
+  <div class="w-full sm:w-5/12 md:5/12 p-2 mb-2 border-2 border-green-400 h-48">
     <!-- Description of food item -->
-    <div class="w-full font-semibold mb-2 text-wrap">
+    <div class="w-full font-semibold mb-2 truncate">
       {{ fooditem.Desc1 }}
     </div>
     <hr />
     <div class="pt-2">
       Brand:
       <!-- If the brand has a link, include it | else just show the brand name -->
-      <a
-        v-if="fooditem.Brand.WebAddress"
-        :href="fooditem.Brand.WebAddress"
-        class="text-green-400 hover:text-green-500"
-        >{{ fooditem.Brand.Desc1 }}</a
-      ><span v-else>{{ fooditem.Brand.Desc1 }}</span>
+      <div v-if="fooditem.Brand.WebAddress" class="inline-block">
+        <a
+          :href="fooditem.Brand.WebAddress"
+          class="text-green-400 hover:text-green-500"
+          >{{ fooditem.Brand.Desc1 }}</a
+        >
+      </div>
+      <div v-else class="inline-block">N/A</div>
     </div>
     <!-- ABBScore -->
     <div class="pt-2">ABBScore: {{ fooditem.ABBScore }}</div>
